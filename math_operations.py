cd1965b750e1fcf9b6ff708676ce21e6263e4f19
sashatsap <77918425+sashatsap@users.noreply.github.com>
2021-03-15 19:16:18 +0200

    optimize math operations

--- math_operations.py
+++ math_operations.py
@@ -6,22 +6,19 @@ product_between_all_even_numbers = 1
 num = [3, 7, -2, 10, 6, -5, 1]
 
 for i in range(len(num)):
+
 	if num[i] < 0:
 		sum_of_negative_elms += num[i]
 
-for i in range(len(num)):
+	if num[i] % 2 == 0:
+		product_between_all_even_numbers *= num[i]
+
 	if num[i] < the_smallest_num:
 		the_smallest_num = num[i]
-
-for i in range(len(num)):
-	if num[i] > the_biggest_num:
+	elif num[i] > the_biggest_num:
 		the_biggest_num = num[i]
 
 product_between_min_and_max = the_smallest_num * the_biggest_num
-
-for i in range(len(num)):
-	if num[i] % 2 == 0:
-		product_between_all_even_numbers *= num[i]
 
 print(f'sum of negative elements: {sum_of_negative_elms}')
 print(f'product between min and max: {product_between_min_and_max}')
