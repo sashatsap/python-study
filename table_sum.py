b79ec7becef8a300b2878c545001e624572e7ed5
sashatsap <77918425+sashatsap@users.noreply.github.com>
2021-03-18 13:28:04 +0200

    optimize table sum

--- table_sum.py
+++ table_sum.py
@@ -5,23 +5,17 @@ lines_sum = []
 table_sum = 0
 
 for i in range(len(nums)):
+	line_sum = 0
 	for j in range(len(nums[0])):
 		table_sum += nums[i][j]
 
-for i in range(len(nums)):
-	line_sum = 0
-	for j in range(len(nums[0])):
+		columns_sum[j] += nums[i][j]
+		
 		line_sum += nums[i][j]
+
 	lines_sum.append(line_sum)
 
-for i in range(len(nums)):
-	for j in range(len(nums[0])):
-		columns_sum[j] += nums[i][j]
-
-for i in range(len(nums)):
 	diagonals_sum[0] += nums[i][i]
-
-for i in range(len(nums)):
 	diagonals_sum[1] += nums[i][len(nums) - i - 1]
 
 print(f'sum of columns: {columns_sum}')
