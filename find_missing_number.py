a3c9f392c248232a16deca604632775a830e0de9
sashatsap <77918425+sashatsap@users.noreply.github.com>
2022-04-01 21:38:03 +0300

    optimized find missing numbers

--- find_missing_number.py
+++ find_missing_number.py
@@ -1,12 +1,14 @@
 all_nums = 5
 nums = [3, 1, 4, 5]
 
-for i in range(all_nums):
-	was_found = False
+for i in range(len(nums)):
 	for j in range(len(nums)):
-		if nums[j] == i + 1:
-			was_found = True
+		if nums[i] < nums[j]:
+			temp = nums[j]
+			nums[j] = nums[i]
+			nums[i] = temp
 
-	if was_found == False:
+for i in range(all_nums - 1):
+	if nums[i] != i + 1:
 		print(i + 1)
 		break
