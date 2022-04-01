1224429397ee6d2cf7c80fa73f3e220f6d8d8746
sashatsap <77918425+sashatsap@users.noreply.github.com>
2022-04-01 21:39:50 +0300

    used build-in function sort

--- find_missing_number.py
+++ find_missing_number.py
@@ -1,12 +1,7 @@
 all_nums = 5
 nums = [3, 1, 4, 5]
 
-for i in range(len(nums)):
-	for j in range(len(nums)):
-		if nums[i] < nums[j]:
-			temp = nums[j]
-			nums[j] = nums[i]
-			nums[i] = temp
+nums.sort()
 
 for i in range(all_nums - 1):
 	if nums[i] != i + 1:
