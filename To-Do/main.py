d023389c82bbd816bb99d6c12ba8854befb55e73
Olecksandr <77918425+sashatsap@users.noreply.github.com>
2023-06-21 12:16:15 -0400

    To-Do

---
+++ To-Do/main.py
@@ -0,0 +1,5 @@
+from task import Task
+
+user = Task()
+
+user.add(task=input('Enter To-Do: '))
---
+++ To-Do/task.py
@@ -0,0 +1,14 @@
+from list import List
+
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class Task(List):
+    def add(self, task):
+        self.task = task
+        self.task_list.append(self.task)
+        print(self.task_list)
