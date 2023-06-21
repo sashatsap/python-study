db4dc0532ed20e44cca12f0107c668174d3e4682
Olecksandr <77918425+sashatsap@users.noreply.github.com>
2023-06-21 12:48:28 -0400

    change class task and list

--- To-Do/main.py
+++ To-Do/main.py
@@ -3,3 +3,5 @@ from task import Task
 user = Task()
 
 user.add(task=input('Enter To-Do: '))
+user.add(task=input('Enter To-Do: '))
+user.change(choice=int(input()), task=input('Enter new To-Do: '))
--- To-Do/task.py
+++ To-Do/task.py
@@ -1,14 +1,22 @@
-from list import List
-
-
 class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
 
-class Task(List):
+class Task:
+    def __init__(self):
+        self.task_list = []
+
     def add(self, task):
         self.task = task
         self.task_list.append(self.task)
         print(self.task_list)
+
+    def change(self, choice, task):
+        self.choice = choice
+        try:
+            self.task_list[choice] = task
+            print(self.task_list)
+        except IndexError:
+            print(f'You dont have {choice} task')
